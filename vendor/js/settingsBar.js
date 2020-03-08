7ffc44353ae3f5b0dbae295361a0efab58dcc009
super1ha1 <khue0011@gmail.com>
2020-03-08 10:54:24 +0700

    Add git ignore, remove vendor files from Git

--- vendor/js/settingsBar.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("settings").style.width = "250px";
-  document.querySelector('.settings').style.visibility = 'hidden';
-}
-
-function closeNav() {
-  document.getElementById('settings').style.width = '0px';
-  document.querySelector('.settings').style.visibility = 'visible';
-}
